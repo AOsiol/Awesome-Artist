50742221c474d35cd6fed502f0a25442e4dbc330
Anthony Osiol <funnynoun@gmail.com>
2020-09-27 16:00:03 -0400

    Updates

---
+++ precache-manifest.d0f9764dacabeeb9df6a54708a101927.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c9a3ad88db37721fbdecc2251c74515",
+    "url": "/Awesome-Artist/index.html"
+  },
+  {
+    "revision": "36c2f651861bf60af8bd",
+    "url": "/Awesome-Artist/static/css/main.1cf1f909.chunk.css"
+  },
+  {
+    "revision": "10de70b152951bf482e2",
+    "url": "/Awesome-Artist/static/js/2.5f8071dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Awesome-Artist/static/js/2.5f8071dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36c2f651861bf60af8bd",
+    "url": "/Awesome-Artist/static/js/main.b1ce8b2b.chunk.js"
+  },
+  {
+    "revision": "89672e625d70693733f6",
+    "url": "/Awesome-Artist/static/js/runtime-main.9fe7f756.js"
+  }
+]);
